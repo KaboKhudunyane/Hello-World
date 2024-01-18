d397e7d538f7fac62f9d70fe85d0aeffe28a4c90
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-01-18 13:57:07 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Importing the necessary module from the 'react' library
+import {} from "react";
+
+// Functional component named 'App'
+const App = () => {
+  // Returning JSX (React elements) representing the component's UI
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+// Exporting the 'App' component as the default export of this module
+export default App;
